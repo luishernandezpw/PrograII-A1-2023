2dc8865ab2c5bc29a08c81f4f084fec8bb29c241
Luis Hernandez <sarco_luis@hotmail.com>
2023-02-17 12:03:02 -0600

    Se agrego un tabhost y su pestañas correspondientes, Longitud, monedas y masa

--- app/src/main/java/com/ugb/miapp/MainActivity.java
+++ app/src/main/java/com/ugb/miapp/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TabHost tbh;
     TextView temp;
     Spinner spn;
     Button btn;
@@ -18,14 +20,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tbh = findViewById(R.id.tbhConversores);
+        tbh.setup();
+
+        tbh.addTab(tbh.newTabSpec("longitud").setContent(R.id.tbLongitud).setIndicator("", getDrawable(R.drawable.longitud)));
+        tbh.addTab(tbh.newTabSpec("monedas").setContent(R.id.tbMonedas).setIndicator("", getDrawable(R.drawable.money)));
+        tbh.addTab(tbh.newTabSpec("masa").setContent(R.id.tbMasa).setIndicator("", getDrawable(R.drawable.masa)));
+
         btn=findViewById(R.id.btnConvertir);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                spn = findViewById(R.id.spnDe);
+                spn = findViewById(R.id.spnDeMonedas);
                 int de = spn.getSelectedItemPosition();
 
-                spn = findViewById(R.id.spna);
+                spn = findViewById(R.id.spnAMonedas);
                 int a = spn.getSelectedItemPosition();
 
                 temp = (TextView) findViewById(R.id.txtCantidad);
