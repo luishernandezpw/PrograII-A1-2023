f99e6e634c23ff6a778e19f40c4188723cc60f1c
Luis Hernandez <sarco_luis@hotmail.com>
2023-03-24 12:00:25 -0600

    Se agrego los metodos necesarios para tomar la foto del amigo

--- app/src/main/java/com/ugb/miapp/MainActivity.java
+++ app/src/main/java/com/ugb/miapp/MainActivity.java
@@ -1,17 +1,26 @@
 package com.ugb.miapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     DB db_agenda;
@@ -20,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView temp;
     FloatingActionButton fab;
+    ImageView imgFotoAmigo;
+    Intent tomarFotoAmigoIntent;
+    String urlCompletaImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,10 +51,46 @@ public class MainActivity extends AppCompatActivity {
                     regresarListaAmigos();
                 }
             });
+            imgFotoAmigo = findViewById(R.id.imgFotoAmigo);
+            imgFotoAmigo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    tomarFotoAmigo();
+                }
+            });
         }catch (Exception e){
             Toast.makeText(this, "Error al cargar: "+ e.getMessage(), Toast.LENGTH_LONG).show();
         }
         mostrar_datos_amigos();
+    }
+    private void tomarFotoAmigo(){
+        try{
+            tomarFotoAmigoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if( tomarFotoAmigoIntent.resolveActivity(getPackageManager())!=null ){
+                File fotoAmigo =null;
+                fotoAmigo = crearImagenAmigo();
+                if(fotoAmigo!=null){
+                    Uri urlFotoAmigo = FileProvider.getUriForFile(MainActivity.this, "com.ugb.miapp.fileprovider", fotoAmigo);
+                    tomarFotoAmigoIntent.putExtra(MediaStore.EXTRA_OUTPUT, urlCompletaImg);
+                    startActivityForResult(tomarFotoAmigoIntent, 1);
+                }else{
+                    Toast.makeText(this, "Error no pude crear la foto del amigo...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }catch (Exception ex){
+            Toast.makeText(this, "Error al tomar Foto: "+ ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    private File crearImagenAmigo() throws Exception{
+        String fechaHoraMs = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String fileName = "imagen_"+ fechaHoraMs +"_";
+        File dirAlmacenamiento = getExternalFilesDir(Environment.DIRECTORY_DCIM);
+        if( dirAlmacenamiento.exists()==false ){
+            dirAlmacenamiento.mkdirs();
+        }
+        File image = File.createTempFile(fileName, ".jpg", dirAlmacenamiento);
+        urlCompletaImg = image.getAbsolutePath();
+        return image;
     }
     void mostrar_datos_amigos(){
         try {
