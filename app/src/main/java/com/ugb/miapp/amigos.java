1ff810a34921f9407f40359447762b5dc32d9340
Luis Hernandez <sarco_luis@hotmail.com>
2023-04-14 12:08:56 -0600

    se creo el layout de list_imagenes y las clases de adaptadorImagenes y amigos

---
+++ app/src/main/java/com/ugb/miapp/amigos.java
@@ -0,0 +1,67 @@
+package com.ugb.miapp;
+
+public class amigos {
+    String idAmigo;
+    String nombre;
+    String direccion;
+    String telefono;
+    String email;
+    String urlFoto;
+
+    public amigos(String idAmigo, String nombre, String direccion, String telefono, String email, String urlFoto) {
+        this.idAmigo = idAmigo;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+        this.urlFoto = urlFoto;
+    }
+
+    public String getIdAmigo() {
+        return idAmigo;
+    }
+
+    public void setIdAmigo(String idAmigo) {
+        this.idAmigo = idAmigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUrlFoto() {
+        return urlFoto;
+    }
+
+    public void setUrlFoto(String urlFoto) {
+        this.urlFoto = urlFoto;
+    }
+}
