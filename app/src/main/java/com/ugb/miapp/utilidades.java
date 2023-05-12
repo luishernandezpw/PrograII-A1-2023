85e5e26e25ceff48248b7163a13b4cb24f7c6b17
Luis Hernandez <sarco_luis@hotmail.com>
2023-05-12 12:04:19 -0600

    Se creo la rama whatsapito y se agrego la sincronizacion con firebase

--- app/src/main/java/com/ugb/miapp/utilidades.java
+++ app/src/main/java/com/ugb/miapp/utilidades.java
@@ -1,8 +1,8 @@
 package com.ugb.miapp;
 
 public class utilidades {
-    static String url_consulta= "http://192.168.100.6:5984/db_amigos/_design/amigos/_view/amigos";
-    static  String url_mto = "http://192.168.100.6:5984/db_amigos/";
+    static String url_consulta= "http://192.168.80.112:5984/db_amigos/_design/amigos/_view/amigos";
+    static  String url_mto = "http://192.168.80.112:5984/db_amigos/";
 
     public String generarIdUnico(){
         return java.util.UUID.randomUUID().toString();
